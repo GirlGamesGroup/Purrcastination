618267986b9b3b1b7799b13084a03c650cefedc7
Perla Peralta <perlap.03@gmail.com>
2018-11-03 18:23:31 -0300

    Whack A Rat
    killing rats yey

---
+++ Assets/Scripts/WhackARatto/RattoMovementScript.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RattoMovementScript : MonoBehaviour {
+	private int currentPlace;
+	private bool[] hasRattoTemp;
+	private RattoSpawner RS;
+	private bool hasBeenCreated;
+	private SpriteRenderer SpRen;
+
+	private Vector3 EndPos;
+	private Vector3 StartPos;
+	private Vector3 MiddlePos;
+	[SerializeField] protected float speedRatto;
+	private float step;
+
+	private bool isAlreadyDying;
+
+
+	void Awake(){
+		step = speedRatto * Time.deltaTime;
+	}
+
+	void Start () {
+		isAlreadyDying = false;
+
+		SpRen = GetComponent<SpriteRenderer> ();
+		RS =GameObject.FindGameObjectWithTag("RattoSpawner").GetComponent<RattoSpawner> ();
+
+		if (gameObject.transform.position == new Vector3 (-2.75f, 0.39f, -0.61f)) {
+			currentPlace = 0;
+		} else if (gameObject.transform.position == new Vector3 (0.42f, -1.83f, -0.61f)) {
+			currentPlace = 1;
+		} else {
+			currentPlace = 2;
+		}
+
+		StartPos = gameObject.transform.position;
+		EndPos = gameObject.transform.position;
+		EndPos.y += 2f;
+
+		StartCoroutine (LifeCycle());
+	}
+	
+	IEnumerator LifeCycle(){
+		yield return new WaitForSeconds(0.3f);
+		while (transform.position != EndPos) {
+			transform.position = Vector3.MoveTowards (transform.position, EndPos, step);
+			yield return new WaitForEndOfFrame ();
+		}
+
+		yield return new WaitForSeconds(0.1f);
+		SpRen.flipX = true;
+		yield return new WaitForSeconds(0.1f);
+		SpRen.flipX = false;	
+		yield return new WaitForSeconds(0.1f);
+		SpRen.flipX = true;
+		yield return new WaitForSeconds(0.1f);
+		SpRen.flipX = true;
+		while (transform.position != StartPos) {
+			transform.position = Vector3.MoveTowards (transform.position, StartPos, step);
+			yield return new WaitForEndOfFrame ();
+		}
+
+		RS.killRatto (currentPlace);
+		Debug.Log ("Died on his own");
+		Destroy(gameObject);
+	}
+
+	private void Die(){
+		RS.killRatto (currentPlace);
+		Debug.Log ("Killed by player");
+		Destroy(gameObject);
+	}
+
+	void OnMouseDown ()
+	{
+		if (!isAlreadyDying) {
+			StopAllCoroutines ();
+			StartCoroutine (HitCoroutine());
+		}
+
+	}
+
+	IEnumerator HitCoroutine(){
+		if (!isAlreadyDying) {
+			isAlreadyDying = true;
+			StartCoroutine (ShakeAnimation ());
+		}
+		yield return 0;
+	}
+
+	IEnumerator ShakeAnimation(){
+		MiddlePos = gameObject.transform.position;
+		Vector3 LeftPos = gameObject.transform.position;
+		LeftPos.x += 0.2f;
+		Vector3 RightPos =  gameObject.transform.position;
+		RightPos.x -= 0.2f;
+
+		int nTimes = 0;
+		while (nTimes < 2) {
+			transform.position = LeftPos;
+			yield return new WaitForSeconds (0.05f);
+			transform.position = RightPos;
+			nTimes++;
+			yield return new WaitForSeconds (0.05f);
+		}
+
+		transform.position = MiddlePos;
+		StartCoroutine (GoDownAfterDying());
+		}
+
+	IEnumerator GoDownAfterDying(){
+					while (transform.position != StartPos) {
+						transform.position = Vector3.MoveTowards (transform.position, StartPos, step*3f);
+						yield return new WaitForEndOfFrame ();
+		
+					}
+					Die();
+	}
+
+	void Update(){
+
+		
+
+//		}
+		
+	}
+
+}
---
+++ Assets/Scripts/WhackARatto/RattoSpawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RattoSpawner : MonoBehaviour {
+	
+	[SerializeField] protected GameObject Ratto;
+	private Vector3[] SpawnPoints;
+	private bool[] hasRatto;
+	[SerializeField] protected int PeriodOf = 1;
+	private int TotalRattos;
+
+	private bool SpawnedRatto;
+
+	// Use this for initialization
+	void Start () {
+		SpawnedRatto = false;
+
+		SpawnPoints = new Vector3[3];
+		hasRatto = new bool[3];
+		TotalRattos = 0;
+
+		SpawnPoints [0] = new Vector3 (-2.75f, 0.39f, -0.61f);
+		SpawnPoints [1] = new Vector3 (0.42f, -1.83f, -0.61f);
+		SpawnPoints [2] = new Vector3 (3.44f, 0.39f, -0.61f);
+
+		for (int i = 0; i < 3; i++) {
+			hasRatto [i] = false;
+		}
+	}
+
+	private void OnEnable () {
+		InvokeRepeating ("SpawnRat", 1, PeriodOf);
+	}
+
+	private void SpawnRat() {
+		SpawnedRatto = false;
+		int randomHole;
+		while (!SpawnedRatto && TotalRattos < 3) {
+			randomHole = Random.Range (0, 3);
+			if (!hasRatto [randomHole]) {
+				Instantiate (Ratto, SpawnPoints [randomHole], Quaternion.identity);
+				hasRatto [randomHole] = true;
+				SpawnedRatto = true;
+				TotalRattos++;
+
+			} else {
+				SpawnedRatto = false;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void killRatto(int i){
+		hasRatto [i] = false;
+		TotalRattos--;
+	}
+
+}
