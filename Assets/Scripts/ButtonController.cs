79c9f5e9d3db727eacb9d163d5af5c86a6e93678
Sol <sol.sanchez.porto@gmail.com>
2018-11-03 16:49:00 -0500

    MiniGamesMenu asset's added and halo

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour {
 
+    [SerializeField] SpriteRenderer img;
 	public void ChangeScene(string scene)
     {
         SceneManager.LoadScene(scene);
+    }
+    public void HaloAppear()
+    {
+        img.enabled = true;
+    }
+    public void HaloDisappear()
+    {
+        img.enabled = false;
     }
 }
