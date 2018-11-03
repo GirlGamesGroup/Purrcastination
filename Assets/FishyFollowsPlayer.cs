a4f940fff59ff49ade8c8efbd4ba80f3b05edb12
Juliana Montes <j.montes495@uniandes.edu.co>
2018-11-03 13:38:46 -0300

    fishies now get back :3

--- Assets/FishyFollowsPlayer.cs
+++ Assets/FishyFollowsPlayer.cs
@@ -5,28 +5,32 @@ using UnityEngine.AI;
 
 public class FishyFollowsPlayer : MonoBehaviour {
 
+    Vector3 initialPosition;
     void Start () {
-
+        initialPosition = transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(transform.position != Camera.main.ScreenToWorldPoint(Input.mousePosition))
+        Vector3 direccion = Vector3.zero;
+        if (Mathf.Abs(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x) > 5f)
         {
-            Vector3 direccion = Vector3.MoveTowards(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.03f);
-            if(direccion.x > transform.position.x && transform.localScale.x > 0)
-            {
-                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-            }
-            else if (direccion.x < transform.position.x && transform.localScale.x < 0)
-            {
-                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-            }
+            direccion = Vector3.MoveTowards(transform.position, initialPosition, 0.01f);
+        }
+        else
+        {
+            direccion = Vector3.MoveTowards(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.03f);
+        }
 
-            transform.position = new Vector3(direccion.x, direccion.y, 0);
+        if (direccion.x > transform.position.x && transform.localScale.x > 0)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
-        else{
-            Debug.Log(":v");
+        else if (direccion.x < transform.position.x && transform.localScale.x < 0)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
+
+        transform.position = new Vector3(direccion.x, direccion.y, 0);
     }
 }
