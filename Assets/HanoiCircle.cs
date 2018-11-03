39b98e18e6cd2f89a88cb2e374036fc5a5feb890
Juliana Montes <j.montes495@uniandes.edu.co>
2018-11-03 00:16:51 -0300

    First version of hanoi towers (needs tweeking)

---
+++ Assets/HanoiCircle.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HanoiCircle : MonoBehaviour
+{
+
+    bool insidePole = false;
+
+    bool collidingWithFloor = false;
+
+    bool isPressed = false;
+
+    private Rigidbody rb;
+
+    Vector3 positionCurrentPole;
+
+    Vector3 positionCurrentTable;
+
+    float threshold = 3f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            DragDisc();
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        string tagCollision = collision.gameObject.tag;
+        if(tagCollision.Equals("Pole"))
+        {
+            IsInPole(collision.gameObject.transform.position);
+        }
+        else if(tagCollision.Equals("Out"))
+        {
+            StoppedBeingInPole();
+        }
+        else if(tagCollision.Equals("Table"))
+        {
+            IsTouchingTable(collision.gameObject.transform.position);
+        }
+        else if (tagCollision.Equals("Base"))
+        {
+            IsTouchingTable(collision.gameObject.transform.position);
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        string tagCollision = collision.gameObject.tag;
+        if (tagCollision.Equals("Table"))
+        {
+            StoppedTouchingTable();
+        }
+        else if (tagCollision.Equals("Base"))
+        {
+            StoppedTouchingTable();
+        }
+    }
+
+    public void IsInPole(Vector3 pole)
+    {
+        if(!insidePole)
+        {
+            positionCurrentPole = pole;
+            insidePole = true;
+        }
+    }
+
+    public void IsTouchingTable(Vector3 table)
+    {
+        positionCurrentTable = table;
+        collidingWithFloor = true;
+    }
+
+    public void StoppedBeingInPole()
+    {
+        insidePole = false;
+    }
+
+    public void StoppedTouchingTable()
+    {
+        collidingWithFloor = false;
+    }
+
+    private void DragDisc()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float x = mousePosition.x;
+        float y = mousePosition.y;
+
+        if (insidePole)
+        {
+            x = positionCurrentPole.x;
+        }
+
+        if(collidingWithFloor && mousePosition.y < positionCurrentTable.y)
+        {
+            y = positionCurrentTable.y;
+        }
+
+        rb.position = new Vector3(x, y, 0);
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+    }
+}
